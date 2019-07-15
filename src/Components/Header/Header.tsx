17d69e1dde530a494878c2ce96ff6b1e73b34af4
shelrod24 <35418412+shelrod24@users.noreply.github.com>
2019-07-15 18:56:30 +1200

    Add project files and folders

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+﻿import * as React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+            <div className="header">
+                <h1 className='headerText'>Guess The Age</h1>
+            </div>
+    )
+}
